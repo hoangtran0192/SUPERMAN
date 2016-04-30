bd7977793cd81a46f73f01d8abfad8b6ad9f97c3
HOANG TRAN <hoangtran0192@gmail.com>
2016-04-30 09:32:10 +0700

    tao  bai tap dau tien

---
+++ Draft.py
@@ -0,0 +1,16 @@
+from turtle import*
+shape("turtle")
+color("blue")
+speed(0)
+d = 50
+
+for i in range(6):
+    for x in range(5):
+        forward(d)
+        left(60)
+    right(150)
+    forward(d)
+    right(90)
+    
+    
+    
